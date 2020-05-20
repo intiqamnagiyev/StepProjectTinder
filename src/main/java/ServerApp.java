7617c16b7b42623b09005afbb6f6749bd420cec5
Intiqam <nagiyevintiqam@gmail.com>
2020-05-20 21:49:34 +0400

    f

--- src/main/java/ServerApp.java
+++ src/main/java/ServerApp.java
@@ -1,6 +1,5 @@
 import dao.MessageDao;
 import dao.UserDao;
-import db.ConnDetails;
 import db.DbConn;
 import db.DbSetup;
 import filter.LikedFilter;
@@ -25,33 +24,32 @@ public class ServerApp {
     private static final EnumSet<DispatcherType> ft = EnumSet.of(DispatcherType.REQUEST);
 
     public static void main(String[] args) throws Exception {
-       //DbSetup.migrate(ConnDetails.url, ConnDetails.username, ConnDetails.password);
-         Connection connection = DbConn.create(ConnDetails.url, ConnDetails.username, ConnDetails.password);
+        //DbSetup.migrate(ConnDetails.url, ConnDetails.username, ConnDetails.password);
+        //  Connection connection = DbConn.create(ConnDetails.url, ConnDetails.username, ConnDetails.password);
 
-      //DbSetup.migrate(HerokuEnv.jdbc_url(), HerokuEnv.jdbc_username(), HerokuEnv.jdbc_password());
-    // Connection connection = DbConn.createFromURL(HerokuEnv.jdbc_url(),HerokuEnv.jdbc_username(),HerokuEnv.jdbc_password());
+        DbSetup.migrate(HerokuEnv.jdbc_url(), HerokuEnv.jdbc_username(), HerokuEnv.jdbc_password());
+        Connection connection = DbConn.createFromURL(HerokuEnv.jdbc_url(), HerokuEnv.jdbc_username(), HerokuEnv.jdbc_password());
 
         Server server = new Server(HerokuEnv.port());
         TemplateEngine engine = TemplateEngine.folder("./templates");
 
         ServletContextHandler handler = new ServletContextHandler();
         handler.addServlet(new ServletHolder(new StaticContentServlet("./templates")), "/static/*");
-        handler.addServlet(new ServletHolder(new RegisterServlet(new UserService(new UserDao(connection)))), "/register/*");
+        handler.addServlet(new ServletHolder(new RegisterServlet(new UserService(new UserDao(connection)))), "/register");
         handler.addServlet(LoginServlet.class, "/login/*");
-        handler.addServlet(new ServletHolder(new UsersServlet(new UserService(new UserDao(connection)), engine)), "/users/*");
-        handler.addServlet(new ServletHolder(new LikedServlet(new UserService(new UserDao(connection)), engine)), "/liked/*");
+        handler.addServlet(new ServletHolder(new UsersServlet(new UserService(new UserDao(connection)), engine)), "/users");
+        handler.addServlet(new ServletHolder(new LikedServlet(new UserService(new UserDao(connection)), engine)), "/liked");
         handler.addServlet(new ServletHolder(new MessagesServlet(engine,
                 new MessageService(new MessageDao(connection)),
-                new UserService(new UserDao(connection)))),"/messages/*");
-        handler.addServlet(LogoutServlet.class,"/logout/*");
+                new UserService(new UserDao(connection)))), "/messages");
+        handler.addServlet(LogoutServlet.class, "/logout");
 
 
         handler.addFilter(new FilterHolder(new LoginFilter(new UserService(new UserDao(connection)))), "/login/*", ft);
         handler.addFilter(new FilterHolder(new UsersFilter()), "/users/*", ft);
-        handler.addFilter(LikedFilter.class, "/liked/*", ft);
-        handler.addFilter(MessagesFilter.class,"/messages/*", ft);
-
-        //handler.addServlet(RedirectServlet.class,"/*");
+        handler.addFilter(LikedFilter.class, "/liked", ft);
+        handler.addFilter(MessagesFilter.class, "/messages", ft);
+        handler.addServlet(RedirectServlet.class, "/*");
         server.setHandler(handler);
         server.start();
         server.join();
